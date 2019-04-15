fd4f6880eed4ad1716fc72764760392791f7ace3
Mariam Gogia <mg@Mariams-MacBook-Pro-2.local>
2019-04-15 17:49:44 -0400

    Initial Commit

---
+++ GroupProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GroupProject
+//
+//  Created by Mariam Gogia on 4/15/19.
+//  Copyright © 2019 Mariam Gogia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
